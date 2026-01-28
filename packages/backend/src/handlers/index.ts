a27639d046dbfcc0094bb9c6eb06da20c189acb9
Matthijs van Henten <440737+mvhenten@users.noreply.github.com>
2026-01-28 01:53:08 +0100

    New endpoints for remit backend

--- packages/backend/src/handlers/index.ts
+++ packages/backend/src/handlers/index.ts
@@ -1,14 +1,17 @@
 import type { OperationHandler, OperationIds } from "../types.js";
+import { ConfigOperations } from "./config.js";
 import { MailboxDetailOperations, MailboxOperations } from "./mailbox.js";
 import { MessageBulkOperations, MessageOperations } from "./message.js";
 import { SyncOperations } from "./sync.js";
-import { ThreadOperations } from "./thread.js";
+import { ThreadDetailOperations, ThreadOperations } from "./thread.js";
 
 // biome-ignore lint/suspicious/noExplicitAny: Types are narrowed downstream
 export const handlers: Record<OperationIds, OperationHandler<any>> = {
+	...ConfigOperations,
 	...MailboxOperations,
 	...MailboxDetailOperations,
 	...SyncOperations,
+	...ThreadDetailOperations,
 	...ThreadOperations,
 	...MessageOperations,
 	...MessageBulkOperations,
